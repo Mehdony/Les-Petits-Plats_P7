1241e899a74638fff9c8326be2056cf93afe33a6
Mehdony <86969900+Mehdony@users.noreply.github.com>
2022-11-28 23:54:37 +0100

    Error message OK

--- index.js
+++ index.js
@@ -33,7 +33,16 @@ document.querySelector(".main-search").addEventListener("input", (e) => {
     mainSearch = true;
     console.log(filteredRecipes);
     document.querySelector(".recipes-container").innerHTML = "";
+  if (filteredRecipes.length === 0) {
+    document.querySelector(".recipes-container").innerHTML = `
+    <div class="no-result">
+      <p>Aucune recette ne correspond à votre critère… vous pouvez chercher « tarte aux pommes », « poisson », etc.</p>
+    </div>
+    `;
+  
+  } else {
     refreshUI();
+  }
     if (str.length <= 3 ) {
       mainSearch = false;
       filteredRecipes = datas.recipes;
